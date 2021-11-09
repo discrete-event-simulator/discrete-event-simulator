5c3a6a6d81b56b8cd304e57df83a0d9cd0ac3e2a
Youngchan Je <ochanje210@gmail.com>
2021-11-09 03:24:16 -0500

    simple event emit to main process

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const Hello = () => {
-  return <div />;
+  useEffect(() => {
+    (window as any).electron.ipcRenderer.on(
+      'reply:create-packet',
+      (data: any) => {
+        console.log('received', data);
+      }
+    );
+  }, []);
+
+  const handleClickCreatePacket = () => {
+    (window as any).electron.ipcRenderer.send('create-packet', [
+      'hello',
+      'test',
+      'any number here',
+    ]);
+  };
+  return (
+    <div>
+      <button type="button" onClick={handleClickCreatePacket}>
+        create a packet
+      </button>
+    </div>
+  );
 };
 
 export default function App() {
