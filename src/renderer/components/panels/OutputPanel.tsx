459abfe2c91b95a8dda335e00db35295fb1396f3
Youngchan Je <ochanje210@gmail.com>
2022-02-15 00:19:59 -0500

    better dragger design

--- src/renderer/components/panels/OutputPanel.tsx
+++ src/renderer/components/panels/OutputPanel.tsx
@@ -1,8 +1,31 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
+import { makeStyles } from '@material-ui/core';
 import { DragHandle } from '@mui/icons-material';
 import { SvgIcon } from '@mui/material';
 import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { AppContext } from 'renderer/pages/HomePage';
+
+const useStyles = makeStyles(() => ({
+  dragger: {
+    border: 'none',
+    width: '100%',
+    height: '3px',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    background: 'lightgray',
+    cursor: 'row-resize',
+    transition: 'height 0.3s, opacity 0.3s',
+    opacity: 0.5,
+    '&:hover': {
+      height: '10px',
+      opacity: 1,
+    },
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 
 const OutputPanel = () => {
   const { simulationData } = useContext(AppContext);
@@ -22,6 +45,8 @@ const OutputPanel = () => {
   useEffect(() => {
     console.log({ simulationData });
   }, [simulationData]);
+
+  const classes = useStyles();
 
   const mouseupEvent = useCallback(() => {
     isDragging.current = false;
@@ -53,19 +78,10 @@ const OutputPanel = () => {
       <button
         type="button"
         ref={ref}
-        style={{
-          border: 'none',
-          width: '100%',
-          height: '30px',
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          background: 'lightgray',
-          cursor: 'row-resize',
-        }}
+        className={classes.dragger}
         onMouseDown={handler}
       >
-        <SvgIcon component={DragHandle} />
+        <SvgIcon style={{ opacity: 0.5 }} component={DragHandle} />
       </button>
       <div
         style={{
