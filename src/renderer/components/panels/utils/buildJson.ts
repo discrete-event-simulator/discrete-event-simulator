66bec752c65631570ad9f131ba8eedc5a903f827
Steven Zhao <stevenwjzhao@hotmail.com>
2022-03-23 23:59:57 -0400

    Fixed ServerMonitor

--- src/renderer/components/panels/utils/buildJson.ts
+++ src/renderer/components/panels/utils/buildJson.ts
@@ -7,11 +7,16 @@ const buildJson = (elements) => {
   };
 
   elements
-    .filter((element) => !element?.source && element?.data.type !== 'Start')
+    .filter(
+      (element) =>
+        !element?.source &&
+        element?.data.type !== 'ServerMonitor' &&
+        element?.data.type !== 'Start'
+    )
     .forEach((element) => {
       if (element.data.type === 'TCPPacketGenerator') {
         const tcpPacketGeneratorId = element.data.label.split(' ')[1];
-        const flowComponentName =`Flow_${tcpPacketGeneratorId}`;
+        const flowComponentName = `Flow_${tcpPacketGeneratorId}`;
         const tcpCubicComponentName = `TCP_Cubic_${tcpPacketGeneratorId}`;
 
         const flowObj = {
@@ -19,28 +24,37 @@ const buildJson = (elements) => {
           type: 'Flow',
           attributes: {
             ...Object.keys(element.data.configs)
-              .filter(key => key.startsWith('flow_'))
-              .reduce((acc, cur) => ({...acc, [cur.replace('flow_', '')]: element.data.configs[cur]}), {}),
+              .filter((key) => key.startsWith('flow_'))
+              .reduce(
+                (acc, cur) => ({
+                  ...acc,
+                  [cur.replace('flow_', '')]: element.data.configs[cur],
+                }),
+                {}
+              ),
             src: `flow_${tcpPacketGeneratorId}`,
             dst: `flow_${tcpPacketGeneratorId}`,
           },
-        }
+        };
         const ccObj = {
           name: tcpCubicComponentName,
           type: 'TCPCubic',
           attributes: {},
-        }
+        };
         const tcpPacketGeneratorObj = {
           name: element.data.label.split(' ').join('_'),
           type: element.data.type,
           attributes: {
             ...Object.keys(element.data.configs)
-              .filter(key => !key.startsWith('flow_'))
-              .reduce((acc, cur) => ({...acc, [cur]: element.data.configs[cur]}), {}),
+              .filter((key) => !key.startsWith('flow_'))
+              .reduce(
+                (acc, cur) => ({ ...acc, [cur]: element.data.configs[cur] }),
+                {}
+              ),
             flow: { reference: flowComponentName },
             cc: { reference: tcpCubicComponentName },
           },
-        }
+        };
         json.components.push(ccObj);
         json.components.push(flowObj);
         json.components.push(tcpPacketGeneratorObj);
@@ -52,9 +66,32 @@ const buildJson = (elements) => {
             ...element.data.configs,
           },
         };
-  
+
         json.components.push(eObj);
       }
+    });
+
+  // ServerMonitor initialized last
+  elements
+    .filter(
+      (element) => !element?.source && element?.data.type === 'ServerMonitor'
+    )
+    .forEach((element) => {
+      const eObj = {
+        name: element.data.label.split(' ').join('_'),
+        type: element.data.type,
+        attributes: {
+          ...element.data.configs,
+        },
+      };
+
+      if (Object.keys(eObj['attributes']).includes('server')) {
+        eObj['attributes']['server'] = {
+          reference: eObj['attributes']['server'],
+        };
+      }
+
+      json.components.push(eObj);
     });
   elements
     .filter((element) => element?.source)
